a4d21f995ee0e63169262c25f99a921c2cb8d7df
ntzolov <ntzolov@gmail.com>
2024-03-07 22:16:12 +0200

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -11,14 +11,14 @@ export async function POST(req) {
   const hashedPassword = await bcrypt.hash(password, 12);
 
   try {
-    const newUser = await User.create({
+    await User.create({
       name,
       email,
       password: hashedPassword,
     });
 
-    console.log('user created!', newUser);
-    return NextResponse.json(newUser, { status: 201 });
+    console.log('User created!', newUser);
+    return NextResponse.json({ success: 'Registration Successful!' }, { status: 201 });
   } catch (err) {
     console.log(err);
     return NextResponse.json({ err: err.message }, { status: 500 });
