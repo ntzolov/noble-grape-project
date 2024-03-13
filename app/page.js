dbe1eb99fed1bf50f607109486fa2fc427250187
ntzolov <ntzolov@gmail.com>
2024-03-13 21:27:57 +0200

    home page updated

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import Hero from '@/components/Hero/Hero';
 import styles from './page.module.scss';
+import { Test } from '@/components/Test/Test';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Test />
+    </>
+  );
 }
