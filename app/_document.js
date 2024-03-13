f8875e7aef1944c8ce7d6619608aa795a53a5ba9
ntzolov <ntzolov@gmail.com>
2024-03-13 21:27:00 +0200

    added fonts in head tag

--- app/_document.js
+++ app/_document.js
@@ -7,9 +7,8 @@ export default function Document() {
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
         <link
-          href='https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap'
-          rel='stylesheet'
-        />
+          href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
+          rel='stylesheet'></link>
       </Head>
       <body>
         <Main />
