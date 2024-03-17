559dc396009cf199fcf50e1c659e7f4bb88a98e1
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-17 22:51:07 +0200

    Update Test.js

--- components/Test/Test.js
+++ components/Test/Test.js
@@ -3,7 +3,7 @@ import styles from './Test.module.scss';
 export function Test() {
   return (
     <div className={styles['test']}>
-      <h1 className={styles['test2']}>THIS SHOULD BE THE NEXT SECTION!</h1>
+      <h1 className={styles['test2']}>THIS SHOULD BE THE NEXT SECTION!!!</h1>
     </div>
   );
 }
