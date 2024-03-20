d0f2755883142a565414953494f7cd8f4bd4b489
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-20 21:04:24 +0200

    added some logic if user is authenticated

--- components/TopNav/TopNav.js
+++ components/TopNav/TopNav.js
@@ -6,6 +6,7 @@ import { useSession, signOut } from 'next-auth/react';
 
 export default function TopNav() {
   const { data, status, loading } = useSession();
+  // console.log({ data, status });
 
   return (
     <nav className={styles['navigation']}>
@@ -17,18 +18,39 @@ export default function TopNav() {
             </Link>
           </div>
         </li>
-        <div className={styles['top-nav--auth']}>
-          <li>
-            <div className={styles['link']}>
-              <Link href='/login'>LOGIN</Link>
+        {status === 'authenticated' ? (
+          <>
+            <div className={styles['top-nav--profile']}>
+              <li>
+                <div className={styles['link']}>
+                  <Link href='/dashboard/user'>{data?.user?.name}</Link>
+                </div>
+              </li>
             </div>
-          </li>
-          <li>
-            <div className={styles['link']}>
-              <Link href='/register'>REGISTER</Link>
+            <div className={styles['top-nav--auth']}>
+              <li>
+                <div className={styles['link']}>
+                  <a className='pointer' onClick={() => signOut({ callbackUrl: '/login' })}>
+                    LOGOUT
+                  </a>
+                </div>
+              </li>
             </div>
-          </li>
-        </div>
+          </>
+        ) : (
+          <div className={styles['top-nav--auth']}>
+            <li>
+              <div className={styles['link']}>
+                <Link href='/login'>LOGIN</Link>
+              </div>
+            </li>
+            <li>
+              <div className={styles['link']}>
+                <Link href='/register'>REGISTER</Link>
+              </div>
+            </li>
+          </div>
+        )}
       </ul>
     </nav>
   );
