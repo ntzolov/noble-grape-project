428aca7c2ee2f515239fee0867e29f30e68ca025
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-20 20:32:30 +0200

    env variables added

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-import { config } from './config.js';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DB_URI: config.DB_URI,
+    DB_URI: 'mongodb+srv://ntzolov:zZYLb8iwd7rDyaac@movie-characters.vtrawke.mongodb.net/nobleGrape?retryWrites=true&w=majority',
+    API: process.env.NODE_ENV === 'production' ? 'http://localhost:3000' : 'http://localhost:3000', // have to change production link when deployed
+    NEXTAUTH_SECRET: 'fsaG!#g3egAgde!gDAGdagE!gE!GDGA',
   },
 };
 
