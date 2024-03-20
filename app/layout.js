28949c4986f7e2586984536f2453065f353edb8d
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-20 20:32:16 +0200

    added SessionProvider

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,32 @@
 import TopNav from '@/components/TopNav/TopNav';
 import Footer from '@/components/Footer/Footer';
+import { Toaster } from 'react-hot-toast';
+import $SessionProvider from '@/context/sessionProvider';
 
 import './globals.scss';
 
-export const metadata = {
-  title: 'Noble Grape',
-  description: 'Online shop for fine and luxury wines.',
-};
+// export const metadata = {
+//   title: 'Noble Grape',
+//   description: 'Online shop for fine and luxury wines.',
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>
-        <div className='main-container'>
-          <TopNav />
-          <main>{children}</main>
-          <Footer />
-        </div>
-      </body>
+      <$SessionProvider>
+        <body suppressHydrationWarning={true}>
+          <div className='main-container'>
+            <TopNav />
+            <Toaster
+              toastOptions={{
+                className: 'toaster',
+              }}
+            />
+            <main>{children}</main>
+            <Footer />
+          </div>
+        </body>
+      </$SessionProvider>
     </html>
   );
 }
