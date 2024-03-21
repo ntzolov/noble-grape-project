7759fd7c801e96dc364cd4aa3555a651a7fc4d32
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-21 21:49:15 +0200

    create global not-found

---
+++ app/not-found.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className='system-wrapper'>
+      <div className='not-found'>
+        <p>404</p>
+        <h2>Oops, The Page you are looking for can&apos;t be found!</h2>
+        <Link className='btn' href={'/'}>
+          Return to homepage
+        </Link>
+      </div>
+    </div>
+  );
+}
