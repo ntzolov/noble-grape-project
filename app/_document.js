2811a2a69aeb5059471597b46153e0949b7ce916
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-24 18:46:27 +0200

    update

--- app/_document.js
+++ app/_document.js
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta
+        {/* <meta
           http-equiv='Content-Security-Policy'
           content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://www.google.com"></meta>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' /> */}
         <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
         <link
           href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
-// import 'dotenv/config';
-import config from './config.mjs';
+import 'dotenv/config';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_DB_URI: config.NEXT_PUBLIC_DB_URI,
-    NEXT_PUBLIC_API: config.NEXT_PUBLIC_API,
-    NEXT_PUBLIC_NEXTAUTH_SECRET: config.NEXT_PUBLIC_NEXTAUTH_SECRET,
-    NEXT_PUBLIC_NEXTAUTH_URL: config.NEXT_PUBLIC_NEXTAUTH_URL,
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: config.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: config.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-  },
+  // env: {
+  //   NEXT_PUBLIC_DB_URI: config.NEXT_PUBLIC_DB_URI,
+  //   NEXT_PUBLIC_API: config.NEXT_PUBLIC_API,
+  //   NEXT_PUBLIC_NEXTAUTH_SECRET: config.NEXT_PUBLIC_NEXTAUTH_SECRET,
+  //   NEXT_PUBLIC_NEXTAUTH_URL: config.NEXT_PUBLIC_NEXTAUTH_URL,
+  //   NEXT_PUBLIC_GOOGLE_CLIENT_ID: config.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  //   NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: config.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+  // },
 };
 
 export default nextConfig;
