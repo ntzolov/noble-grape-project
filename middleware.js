eb6294548281818e6f213ba69c77e23566ae7a7a
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-24 15:53:46 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,25 @@
-export { default } from 'next-auth/middleware';
+// export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
-export const config = { matcher: ['/dashboard/:path*'] };
+export const config = { matcher: ['/dashboard/:path*', '/api/user/:path*', '/api/admin/:path*'] };
+
+export default withAuth(
+  async function middleware(req) {
+    const url = req.nextUrl.pathname;
+    const userRole = req?.nextauth?.token?.user?.role;
+
+    if (url?.includes('/admin') && userRole !== 'admin') {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        if (!token) {
+          return false;
+        }
+      },
+    },
+  }
+);
