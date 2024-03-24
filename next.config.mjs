3e11c757ab7cd93b55186dcd2a047f9255a3018e
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-24 17:38:05 +0200

    update environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
-import 'dotenv/config';
+// import 'dotenv/config';
+import config from './config.mjs';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    DB_URI: config.DB_URI,
+    API: config.API,
+    NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: config.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: config.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: config.GOOGLE_CLIENT_SECRET,
+  },
+};
 
 export default nextConfig;
