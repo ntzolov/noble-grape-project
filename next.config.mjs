21d31082ec383a884a7b60881df7c2357f132616
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-24 18:50:20 +0200

    Revert "update environment"
    This reverts commit 3e11c757ab7cd93b55186dcd2a047f9255a3018e.

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-// import 'dotenv/config';
-import config from './config.mjs';
+import 'dotenv/config';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    DB_URI: config.DB_URI,
-    API: config.API,
-    NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: config.NEXTAUTH_URL,
-    GOOGLE_CLIENT_ID: config.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: config.GOOGLE_CLIENT_SECRET,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
