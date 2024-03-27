2e5e87eae2515abd5c781cd1f1f346c8a81bba44
Nikolay Tzolov <ntzolov@gmail.com>
2024-03-27 21:16:32 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import mongooseUniqueValidator from 'mongoose-unique-validator';
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
